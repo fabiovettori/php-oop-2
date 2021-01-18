db0ad4f5badbde9ab170dac08911cb3da8e61f61
Fabio Vettori <fabio_vettori@hotmail.it>
2021-01-18 22:23:52 +0100

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// ESERCIZIO
+// definire la struttura di una tabella users che contiene i dati degli utenti di un blog
+// definire una classe User che "mappa" la tabella users
+// creare alcune istanze della classe User per stampare i dati degli utenti
+// Nome repo: php-oop-2
+// N.B: NON dovete creare per davvero il database né la tabella
+
+echo 'test';
+?>
